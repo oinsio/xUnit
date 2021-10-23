83777de172fb7ba0b85b989bc52091aeab94527c
oinsio <oinsio.srv@gmail.com>
2021-10-23 18:27:09 +0300

    implement set_up method

--- xUnit.py
+++ xUnit.py
@@ -2,7 +2,11 @@ class TestCase:
     def __init__(self, name):
         self.name = name
 
+    def set_up(self):
+        pass
+
     def run(self):
+        self.set_up()
         method = getattr(self, self.name)
         method()
 
@@ -15,13 +19,23 @@ class WasRun(TestCase):
     def test_method(self):
         self.wasRun = 1
 
+    def set_up(self):
+        self.wasRun = None
+        self.wasSetUp = 1
+
 
 class TestCaseTest(TestCase):
+    def set_up(self):
+        self.test = WasRun("test_method")
+
     def test_running(self):
-        test = WasRun("test_method")
-        assert not test.wasRun
-        test.run()
-        assert test.wasRun
+        self.test.run()
+        assert self.test.wasRun
+
+    def test_set_up(self):
+        self.test.run()
+        assert self.test.wasSetUp
 
 
 TestCaseTest("test_running").run()
+TestCaseTest("test_set_up").run()

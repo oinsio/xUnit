977ffd0f5167ec7e653ff24178d5c2fb5d2f5b80
oinsio <oinsio.srv@gmail.com>
2021-10-24 12:18:26 +0300

    implement tear_down

--- xUnit.py
+++ xUnit.py
@@ -6,9 +6,19 @@ class TestCase:
         pass
 
     def run(self):
+        result = TestResult()
+        result.test_started()
         self.set_up()
-        method = getattr(self, self.name)
-        method()
+        try:
+            method = getattr(self, self.name)
+            method()
+        except:
+            result.test_failed()
+        self.tear_down()
+        return result
+
+    def tear_down(self):
+        pass
 
 
 class WasRun(TestCase):
@@ -17,20 +27,55 @@ class WasRun(TestCase):
         self.log = None
         TestCase.__init__(self, name)
 
+    def set_up(self):
+        self.log = "set_up "
+
     def test_method(self):
-        self.wasRun = 1
         self.log = self.log + "test_method "
 
-    def set_up(self):
-        self.wasRun = None
-        self.log = "set_up "
+    def tear_down(self):
+        self.log = self.log + "tear_down "
+
+    def test_broken_method(self):
+        raise Exception
+
+
+class TestResult:
+    def __init__(self):
+        self.runCount = 0
+        self.errorCount = 0
+
+    def test_started(self):
+        self.runCount = self.runCount + 1
+
+    def test_failed(self):
+        self.errorCount = self.errorCount + 1
+
+    def summary(self):
+        return "%d run, %d failed" % (self.runCount, self.errorCount)
 
 
 class TestCaseTest(TestCase):
+    def test_result(self):
+        test = WasRun("test_method")
+        result = test.run()
+        assert "1 run, 0 failed" == result.summary()
+
+    def test_failed_result(self):
+        test = WasRun("test_broken_method")
+        result = test.run()
+        assert "1 run, 1 failed" == result.summary()
+
     def test_template_method(self):
         test = WasRun("test_method")
         test.run()
-        assert "set_up test_method " == test.log
+        assert "set_up test_method tear_down " == test.log
+
+    def test_failed_result_formatting(self):
+        result = TestResult()
+        result.test_started()
+        result.test_failed()
+        assert "1 run, 1 failed" == result.summary()
 
 
 TestCaseTest("test_template_method").run()
